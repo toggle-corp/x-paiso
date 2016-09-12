6b15b36057c9bdc696cc53132594a34ce18dd3a1
Frozen Helium <fren.ankit@gmail.com>
2016-09-12 20:29:14 +0545

    Add select people activity

---
+++ app/src/main/java/com/togglecorp/paiso/SelectPeopleActivity.java
@@ -0,0 +1,13 @@
+package com.togglecorp.paiso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectPeopleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_people);
+    }
+}
