d7fa791a598df4bf4b915133651ed47b5cde43c4
Frozen Helium <fren.ankit@gmail.com>
2016-09-12 13:13:21 +0545

    Add navigation drawer

--- app/src/main/java/com/togglecorp/paiso/MainActivity.java
+++ app/src/main/java/com/togglecorp/paiso/MainActivity.java
@@ -1,8 +1,13 @@
 package com.togglecorp.paiso;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -21,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseUser mFirebaseUser;
     private GoogleApiClient mGoogleApiClient;
 
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +46,42 @@ public class MainActivity extends AppCompatActivity {
         } else {
             mUsername = mFirebaseUser.getDisplayName();
         }
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        mNavigationView = (NavigationView)findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                }
+        );
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void logout() {
